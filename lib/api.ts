065f980b03114dd41eeee18056a4d7f75d286b81
SajanaKavishan <trsajanakavishan67@gmail.com>
2026-04-23 23:29:21 +0530

    feat: update API base URL configuration and simplify getApiBaseUrl function

--- lib/api.ts
+++ lib/api.ts
@@ -1,28 +1,5 @@
-import Constants from 'expo-constants';
-import { Platform } from 'react-native';
-
 export const getApiBaseUrl = () => {
-  if (process.env.EXPO_PUBLIC_API_URL) {
-    return process.env.EXPO_PUBLIC_API_URL.replace(/\/$/, '');
-  }
-
-  const hostUri =
-    (Constants as any)?.expoConfig?.hostUri ||
-    (Constants as any)?.manifest2?.extra?.expoClient?.hostUri ||
-    (Constants as any)?.manifest?.debuggerHost;
-
-  if (typeof hostUri === 'string' && hostUri.length > 0) {
-    const host = hostUri.split(':')[0];
-    if (host) {
-      return `http://${host}:5000/api`;
-    }
-  }
-
-  if (Platform.OS === 'android') {
-    return 'http://10.0.2.2:5000/api';
-  }
-
-  return 'http://localhost:5000/api';
+  return (process.env.EXPO_PUBLIC_API_URL || 'http://localhost:5000/api').replace(/\/$/, '');
 };
 
 export const API_BASE_URL = getApiBaseUrl();
